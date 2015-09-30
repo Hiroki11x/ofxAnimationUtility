f7c388422e7f329c925cb2f9889d99c8790fb3fc
Hiroki Naganuma <hiroki11x@gmail.com>
2015-09-30 21:30:23 +0900

    temp

---
+++ src/StatusCircle.cpp
@@ -0,0 +1,10 @@
+//
+//  StatusCircle.cpp
+//  ofxAnimationUtility
+//
+//  Created by HirokiNaganuma on 2015/10/01.
+//
+//
+
+#include "StatusCircle.h"
+
---
+++ src/StatusCircle.h
@@ -0,0 +1,22 @@
+//
+//  StatusCircle.h
+//  ofxAnimationUtility
+//
+//  Created by HirokiNaganuma on 2015/10/01.
+//
+//
+
+#ifndef __ofxAnimationUtility__StatusCircle__
+#define __ofxAnimationUtility__StatusCircle__
+
+#include "ofMain.h"
+
+class StatusCircle{
+private:
+    ofVec2f posision;
+    void setup_position(ofVec2f pos);
+public:
+    
+};
+
+#endif /* defined(__ofxAnimationUtility__StatusCircle__) */
